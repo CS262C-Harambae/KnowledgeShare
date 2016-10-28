cecfe90e60961553244efcf4a0888f9ae3598b87
noblecoreyj <noblecoreyj@gmail.com>
2016-10-27 23:31:12 -0400

    Added documentation

--- app/src/main/java/edu/calvin/harambe/knowledgeshare/HarambeActivity.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/HarambeActivity.java
@@ -1,10 +1,22 @@
 package edu.calvin.harambe.knowledgeshare;
 
+/**
+ * Harambe Activity (HarambeActivity.java)
+ * This class provides the workings for the Harambe Activity
+ * (perhaps this will be integrated into AboutActivity.java...)
+ *
+ * @author: Corey Noble (cjn8)
+ * @version: 1.0 (Fall, 2016)
+ */
+
+// Imports
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Activity class
 public class HarambeActivity extends AppCompatActivity {
 
+    // Initialize activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
