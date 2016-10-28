38d6ddc1cf64b1c0da1df7cb7b50029d08409841
noblecoreyj <noblecoreyj@gmail.com>
2016-10-27 23:41:04 -0400

    Added documentation

--- app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsCard.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsCard.java
@@ -1,11 +1,23 @@
 package edu.calvin.harambe.knowledgeshare;
 
+/**
+ * News Card (NewsCard.java)
+ * This class defines a News Card object
+ *
+ * @author: Corey Noble (cjn8)
+ * @version: 1.0 (Fall, 2016)
+ */
+
+// Class
 public class NewsCard {
+    
+    // Instance variables
     private String headline;
     private String story;
     private int color;
     private int category;
 
+    // Constructor
     public NewsCard() {
         this.headline = "Sample Headline";
         this.story = "Sample Story";
@@ -13,6 +25,7 @@ public class NewsCard {
         this.category = 1;
     }
 
+    // Explicit constructor
     public NewsCard(String headline, String story, int color, int category) {
         this.headline = headline;
         this.story = story;
@@ -20,34 +33,42 @@ public class NewsCard {
         this.category = category;
     }
 
+    // Headline accessor
     public String getHeadline() {
         return headline;
     }
 
+    // Headline mutator
     public void setHeadline(String headline) {
         this.headline = headline;
     }
 
+    // Story accessor
     public String getStory() {
         return story;
     }
 
+    // Story mutator
     public void setStory(String story) {
         this.story = story;
     }
 
+    // Color accessor
     public int getColor() {
         return color;
     }
 
+    // Color mutator
     public void setColor(int color){
         this.color = color;
     }
 
+    // Category accessor
     public int getCategory() {
         return category;
     }
 
+    // Category mutator
     public void setCategory(int category) {
         this.category = category;
     }
