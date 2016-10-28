414ef3e570149af13df8a50d2ad5e966172aa74e
noblecoreyj <noblecoreyj@gmail.com>
2016-10-27 23:28:06 -0400

    Added documentation

--- app/src/main/java/edu/calvin/harambe/knowledgeshare/AboutActivity.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/AboutActivity.java
@@ -1,10 +1,21 @@
 package edu.calvin.harambe.knowledgeshare;
 
+/**
+ * About Activity (AboutActivity.java)
+ * This class provides the workings for the About Activity
+ *
+ * @author: Corey Noble (cjn8)
+ * @version: 1.0 (Fall, 2016)
+ */
+
+// Imports
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Activity class
 public class AboutActivity extends AppCompatActivity {
 
+    // Initialize activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
