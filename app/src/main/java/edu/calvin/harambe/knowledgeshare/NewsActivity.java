87f3f33fa51c19981e3aecc7e0911b13b28313fb
noblecoreyj <noblecoreyj@gmail.com>
2016-11-04 00:08:31 -0400

    Refined search and activity open on click

--- app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsActivity.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsActivity.java
@@ -11,28 +11,57 @@ package edu.calvin.harambe.knowledgeshare;
  */
 
 // Imports (Android)
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.text.TextUtils;
 import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 // Imports (Java)
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
 import java.util.List;
 
 // Activity class
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     // Instance variables
+    //private EditText searchBox;
+    //private Button searchButton;
     private RecyclerView recyclerView;
-    private NewsCardAdapter adapter;
-    private List<NewsCard> cardList;
+    private SearchView searchView;
+    private NewsCardAdapter mainAdapter;
+    //private NewsCardAdapter displayAdapter;
+    //private NewsCardAdapter searchAdapter;
+    private ArrayList<NewsCard> cardList;
+    //private ArrayList<NewsCard> displayList;
+    //private ArrayList<NewsCard> searchList;
 
     // Initialize activity and build the card list
     @Override
@@ -40,46 +69,131 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        //searchBox = (EditText) findViewById(R.id.searchBar);
+        //searchButton = (Button) findViewById(R.id.searchButton);
         cardList = new ArrayList<>();
-        adapter = new NewsCardAdapter(this, cardList);
+        mainAdapter = new NewsCardAdapter(this, cardList);
+        processCards();
+        //displayList = cardList;
+        //searchList = cardList;
+        //displayAdapter = new NewsCardAdapter(this, displayList);
+        //searchAdapter = new NewsCardAdapter(this, searchList);
+
+        //recyclerView.setAdapter(mainAdapter);
+/*
+        searchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId != EditorInfo.IME_ACTION_SEND) {
+                    String query = searchBox.getText().toString();
+                    searchCards(query);
+                    return true;
+                }
+                return false;
+            }
+        });*/
+        //searchListView = (ListView) findViewById(R.id.searchListView);
+        //searchView = (SearchView) findViewById(R.id.search_view);
+
+        //searchList
+
         RecyclerView.LayoutManager harambeLayoutManager = new GridLayoutManager(this, 1);
         recyclerView.setLayoutManager(harambeLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
-        setupCards();
+        recyclerView.setAdapter(mainAdapter);
+        //searchListView.setAdapter(mainAdapter);
+
+        formatCards();
+    }
+
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mainAdapter.getFilter().filter("");
+        }
+        else {
+            mainAdapter.getFilter().filter(newText.toString());
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
     }
 
     // Add cards (now they are just test cards)
-    private void setupCards() {
-        NewsCard card = new NewsCard("Breaking News", "Calvin strikes oil", 1, 1);
+    private void processCards() {
+        NewsCard card = new NewsCard("Breaking News", "provost@calvin.edu", "Calvin strikes oil", 0, 1);
         cardList.add(card);
 
-        card = new NewsCard("Dance Guild", "You can ignore this announcement", 1, 1);
+        card = new NewsCard("Dance Guild", "random@calvin.edu", "You can ignore this announcement", 0, 1);
         cardList.add(card);
 
-        card = new NewsCard("$8228/#%042734", "Yet another poorly formatted title...", 1, 1);
+        card = new NewsCard("$8228/#%042734", "notReallyWorthLookingAt@calvin.edu", "Yet another poorly formatted title...", 0, 1);
         cardList.add(card);
 
-        card = new NewsCard("FLU CLINIC - Health Services - October 20", "Calvin College Health Services is hosting another flu clinic on Thursday, October 20, from 10 a.m. to 2 p.m. The initial check-in point will be located in Health Services.\nDue to its ineffectiveness, flu mist will not be available this year.  Only the flu shot will be available.", 1, 1);
+        card = new NewsCard("FLU CLINIC - Health Services - October 20", "email", "Calvin College Health Services is hosting another flu clinic on Thursday, October 20, from 10 a.m. to 2 p.m. The initial check-in point will be located in Health Services.\nDue to its ineffectiveness, flu mist will not be available this year.  Only the flu shot will be available.", 0, 1);
         cardList.add(card);
 
-        card = new NewsCard("Philosophy Faculty Book Reception", "The Philosophy Department at Calvin College would like to invite you to attend our Faculty Book Reception, Thursday, October 20, from 3:30 to 5:30 PM\nThe reception will offer students, staff, and faculty the opportunity to hear a short presentation by each of our faculty members on their recent publications.", 1, 1);
+        card = new NewsCard("Philosophy Faculty Book Reception", "NULL", "The Philosophy Department at Calvin College would like to invite you to attend our Faculty Book Reception, Thursday, October 20, from 3:30 to 5:30 PM\nThe reception will offer students, staff, and faculty the opportunity to hear a short presentation by each of our faculty members on their recent publications.", 0, 1);
         cardList.add(card);
 
-        adapter.notifyDataSetChanged();
+        mainAdapter.notifyDataSetChanged();
+    }
+/*
+    public void searchCards(String query) {
+        searchList.clear();
+        for (int i = 0; i < displayList.size(); i++) {
+            if (displayList.get(i).getHeadline().contains(query)) {
+                searchList.add(displayList.get(i));
+            } else if (displayList.get(i).getSender().contains(query)) {
+                searchList.add(displayList.get(i));
+            } else if (displayList.get(i).getStory().contains(query)) {
+                searchList.add(displayList.get(i));
+            }
+        }
+        recyclerView.setAdapter(searchAdapter);
+        searchAdapter.notifyDataSetChanged();
+    }*/
+
+    public void formatCards() {
+        for (int i = 0; i < cardList.size(); i++) {
+            NewsCard currentCard = cardList.get(i);
+            if (currentCard.getSender().equals("provost@calvin.edu")) {
+                currentCard.setColor(1);
+            }
+            else if (currentCard.getSender().equals("random@calvin.edu")) {
+                currentCard.setColor(2);
+            }
+            else if (currentCard.getSender().equals("NULL")) {
+                currentCard.setColor(3);
+            }
+            else if (currentCard.getSender().equals("email")) {
+                currentCard.setColor(4);
+            }
+        }
+        mainAdapter.notifyDataSetChanged();
     }
 
     // Create the options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchActionBarItem = menu.findItem(R.id.searchActionBarItem);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchActionBarItem);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
         return true;
     }
 
     // Go to different activity if selected from the options menu
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.aboutActivity:
                 startActivity(new Intent(NewsActivity.this, AboutActivity.class));
                 return true;
--- app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsCardAdapter.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/NewsCardAdapter.java
@@ -10,6 +10,9 @@ package edu.calvin.harambe.knowledgeshare;
 
 // Imports (Android)
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,37 +20,55 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 // Imports (Java)
+import java.util.ArrayList;
 import java.util.List;
 
 // Adapter class
-public class NewsCardAdapter extends RecyclerView.Adapter<NewsCardAdapter.HarambeViewHolder> {
+public class NewsCardAdapter extends RecyclerView.Adapter<NewsCardAdapter.HarambeViewHolder> implements Filterable {
     
     // Instance variables
     private Context cardContext;
-    private List<NewsCard> cardList;
+    private ArrayList<NewsCard> cardList;
+    private ArrayList<NewsCard> backupList;
 
     // Inner class for making RecyclerView.ViewHolder work for us
     public class HarambeViewHolder extends RecyclerView.ViewHolder {
         
         // Instance variables
         public TextView headline;
+        public TextView sender;
         public TextView story;
+        public ImageView color;
 
         // Explicit constructor
         public HarambeViewHolder(View view) {
             super(view);
             headline = (TextView) view.findViewById(R.id.headline);
+            sender = (TextView) view.findViewById(R.id.sender);
             story = (TextView) view.findViewById(R.id.story);
+            color = (ImageView) view.findViewById(R.id.colorBar);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), NewsCardActivity.class);
+                    intent.putExtra("headlinePass", headline.getText().toString());
+                    intent.putExtra("senderPass", sender.getText().toString());
+                    intent.putExtra("storyPass", story.getText().toString());
+                    v.getContext().startActivity(intent);
+                }
+            });
         }
     }
 
     // Adapter for News Card object
-    public NewsCardAdapter(Context cardContext, List<NewsCard> cardList) {
+    public NewsCardAdapter(Context cardContext, ArrayList<NewsCard> cardList) {
         this.cardContext = cardContext;
         this.cardList = cardList;
     }
@@ -64,12 +85,85 @@ public class NewsCardAdapter extends RecyclerView.Adapter<NewsCardAdapter.Haramb
     public void onBindViewHolder(final HarambeViewHolder holder, int position) {
         NewsCard card = cardList.get(position);
         holder.headline.setText(card.getHeadline());
+        holder.sender.setText(card.getSender());
         holder.story.setText(card.getStory());
+        Integer colorBlack = 0;
+        Integer colorBlue = 1;
+        Integer colorRed = 2;
+        Integer colorPurple = 3;
+        Integer colorGreen = 4;
+        Integer colorYellow = 5;
+        Integer intBlack = Integer.compare(card.getColor(), colorBlack);
+        boolean isBlack = (intBlack != 1);
+        Integer intBlue = Integer.compare(card.getColor(), colorBlue);
+        boolean isBlue = (intBlue != 1);
+        Integer intRed = Integer.compare(card.getColor(), colorRed);
+        boolean isRed = (intRed != 1);
+        Integer intPurple = Integer.compare(card.getColor(), colorPurple);
+        boolean isPurple = (intPurple != 1);
+        Integer intGreen = Integer.compare(card.getColor(), colorGreen);
+        boolean isGreen = (intGreen != 1);
+        Integer intYellow = Integer.compare(card.getColor(), colorYellow);
+        boolean isYellow = (intYellow != 1);
+        if (isBlack) {
+            holder.color.setBackgroundColor(Color.rgb(0, 0, 0));
+        }
+        else if (isBlue) {
+            holder.color.setBackgroundColor(Color.rgb(11, 94, 229));
+        }
+        else if (isRed) {
+            holder.color.setBackgroundColor(Color.rgb(229, 29, 11));
+        }
+        else if (isPurple) {
+            holder.color.setBackgroundColor(Color.rgb(174, 11, 229));
+        }
+        else if (isGreen) {
+            holder.color.setBackgroundColor(Color.rgb(14, 184, 54));
+        }
+        else if (isYellow) {
+            holder.color.setBackgroundColor(Color.rgb(238, 242, 7));
+        }
     }
 
     // Get size of cardList
     @Override
     public int getItemCount() {
         return cardList.size();
+    }
+
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                final FilterResults oReturn = new FilterResults();
+                final ArrayList<NewsCard> results = new ArrayList<NewsCard>();
+                if (backupList == null) {
+                    backupList = cardList;
+                    if (constraint != null) {
+                        if (backupList != null & backupList.size() > 0) {
+                            for (final NewsCard g : backupList) {
+                                if (g.getHeadline().toLowerCase().contains(constraint.toString())) {
+                                    results.add(g);
+                                }
+                                else if (g.getSender().toLowerCase().contains(constraint.toString())) {
+                                    results.add(g);
+                                }
+                                else if (g.getStory().toLowerCase().contains(constraint.toString())) {
+                                    results.add(g);
+                                }
+                            }
+                        }
+                        oReturn.values = results;
+                    }
+                }
+                return oReturn;
+
+            }
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                cardList = (ArrayList<NewsCard>)results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
