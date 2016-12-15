e2aae3904f423e928c748d25859f857435789afc
noblecoreyj <noblecoreyj@gmail.com>
2016-12-15 17:09:12 -0500

    Update SettingsActivity.java
    Documentation

--- app/src/main/java/edu/calvin/harambe/knowledgeshare/SettingsActivity.java
+++ app/src/main/java/edu/calvin/harambe/knowledgeshare/SettingsActivity.java
@@ -19,22 +19,24 @@ import java.util.ArrayList;
  * This activity provides the category filters
  * It populates checkboxes and filter accordingly
  *
- * @version 1.0
+ * @version 1.0.1 (Fall, 2016)
  */
 
 public class SettingsActivity extends AppCompatActivity {
     public StringBuilder tempConstraint = new StringBuilder();
+    
+    /**
+     * onCreate
+     * This method initializes the checkboxes and sets saved preferences
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_settings);
         final SharedPreferences categories = getPreferences(Context.MODE_PRIVATE);
-        //final CheckBox administrationCheck = (CheckBox) findViewById(R.id.administrationCheck);
         final CheckBox campusMinistriesCheck = (CheckBox) findViewById(R.id.campusMinistriesCheck);
         final CheckBox careerDevelopmentCheck = (CheckBox) findViewById(R.id.careerDevelopmentCheck);
         final CheckBox danceGuildCheck = (CheckBox) findViewById(R.id.danceGuildCheck);
-        //final CheckBox humanResourcesCheck = (CheckBox) findViewById(R.id.humanResourcesCheck);
         final CheckBox bookstoreCheck = (CheckBox) findViewById(R.id.bookstoreCheck);
         final CheckBox financesCheck = (CheckBox) findViewById(R.id.financialCheck);
         final CheckBox healthCheck = (CheckBox) findViewById(R.id.healthCheck);
@@ -154,25 +156,24 @@ public class SettingsActivity extends AppCompatActivity {
                 editor.commit();
             }
         });
-        //selectItem();
-        
-        //getFragmentManager().beginTransaction().replace(android.R.id.content, new CategoriesFragment()).commit();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        //selectItem();
     }
 
-
+    /**
+     * applyFilters
+     * This method creates a constraint for SQL based on which check boxes are checked
+     *
+     * @param view, a View
+     */
     public void applyFilters(View view) {
-        //NewsActivity.emailList.clear();
         tempConstraint = new StringBuilder();
         tempConstraint.delete(0, tempConstraint.length());
         NewsActivity.constraint = new StringBuilder();
         NewsActivity.constraint.delete(0, NewsActivity.constraint.length());
-        //tempConstraint.append("'");
         System.out.print("Constraint check 1: " + tempConstraint + "\n");
         ArrayList<String> filterList = new ArrayList<String>();
         filterList.add("amh72@students.calvin.edu");
@@ -283,11 +284,6 @@ public class SettingsActivity extends AppCompatActivity {
             filterList.remove("bj24@calvin.edu");
             filterList.remove("rwt2@calvin.edu");
             filterList.remove("careerstudent@calvin.edu");
-            /*
-            for (int i = 0; i < careerDevelopmentList.size(); i++) {
-                tempConstraint.append(careerDevelopmentList.get(i));
-                tempConstraint.append("%7C");
-            }*/
         }
         if (campusMinistriesChecked) {
             filterList.remove("campusministries@calvin.edu");
@@ -387,71 +383,8 @@ public class SettingsActivity extends AppCompatActivity {
         if (!filterList.isEmpty()) {
             tempConstraint.setLength(tempConstraint.length() - 3);
         }
-        //tempConstraint.append("'");
-        //System.out.print("\n" + tempConstraint + "\n");
         NewsActivity.constraint.append(tempConstraint);
         Toast confirmation = Toast.makeText(getApplicationContext(), "Filters Applied", Toast.LENGTH_SHORT);
         confirmation.show();
     }
-/*
-    public void selectItem() {
-        NewsActivity.emailList.clear();
-        //tempConstraint.append("'");
-        boolean administrationChecked = ((CheckBox) findViewById(R.id.administrationCheck)).isChecked();
-        boolean careerCenterChecked = ((CheckBox) findViewById(R.id.careerDevelopmentCheck)).isChecked();
-        boolean campusMinistriesChecked = ((CheckBox) findViewById(R.id.campusMinistriesCheck)).isChecked();
-        boolean danceGuildChecked = ((CheckBox) findViewById(R.id.danceGuildCheck)).isChecked();
-        boolean humanResourcesChecked = ((CheckBox) findViewById(R.id.humanResourcesCheck)).isChecked();
-        boolean bookstoreChecked = ((CheckBox) findViewById(R.id.bookstoreCheck)).isChecked();
-        if (administrationChecked) {
-            NewsActivity.emailList.add("president@calvin.edu");
-            NewsActivity.emailList.add("brac@calvin.edu");
-            NewsActivity.emailList.add("svploeg@calvin.edu");
-            NewsActivity.emailList.add("kde2@calvin.edu");
-            NewsActivity.emailList.add("rjb42@calvin.edu");
-            NewsActivity.emailList.add("sav36@calvin.edu");
-            NewsActivity.emailList.add("thubers@calvin.edu");
-            NewsActivity.emailList.add("lopa@calvin.edu");
-        }
-        if (careerCenterChecked) {
-            NewsActivity.emailList.add("career@calvin.edu");
-            NewsActivity.emailList.add("kdykhous@calvin.edu");
-            NewsActivity.emailList.add("tdj4@calvin.edu");
-            NewsActivity.emailList.add("cbt4@calvin.edu");
-            NewsActivity.emailList.add("bcok@calvin.edu");
-            NewsActivity.emailList.add("lbl3@calvin.edu");
-            NewsActivity.emailList.add("mec6@calvin.edu");
-            NewsActivity.emailList.add("bspeyers@calvin.edu");
-            NewsActivity.emailList.add("bj24@calvin.edu");
-            NewsActivity.emailList.add("rwt2@calvin.edu");
-        }
-        if (campusMinistriesChecked) {
-            NewsActivity.emailList.add("campusministries@calvin.edu");
-            NewsActivity.emailList.add("stobja@calvin.edu");
-            NewsActivity.emailList.add("pastormary@calvin.edu");
-            NewsActivity.emailList.add("mwp4@calvin.edu");
-            NewsActivity.emailList.add("jr224@calvin.edu");
-            NewsActivity.emailList.add("psr2@calvin.edu");
-            NewsActivity.emailList.add("jcs33@calvin.edu");
-            NewsActivity.emailList.add("rat9@calvin.edu");
-        }
-        if (danceGuildChecked) {
-            NewsActivity.emailList.add("slp33@students.calvin.edu");
-        }
-        if (humanResourcesChecked) {
-            NewsActivity.emailList.add("humanresources@calvin.edu");
-            NewsActivity.emailList.add("briton@calvin.edu");
-            NewsActivity.emailList.add("mag24@calvin.edu");
-            NewsActivity.emailList.add("alg35@calvin.edu");
-            NewsActivity.emailList.add("dhonner@calvin.edu");
-            NewsActivity.emailList.add("thubers@calvin.edu");
-            NewsActivity.emailList.add("emw27@calvin.edu");
-            NewsActivity.emailList.add("ajn8@calvin.edu");
-        }
-        if (bookstoreChecked) {
-            NewsActivity.emailList.add("mjg44@calvin.edu");
-            NewsActivity.emailList.add("cgl4@calvin.edu");
-            NewsActivity.emailList.add("calvin@bkstr.com");
-        }
-    }*/
 }
